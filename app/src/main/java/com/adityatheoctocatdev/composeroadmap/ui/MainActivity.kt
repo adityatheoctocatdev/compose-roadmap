88ba728da681da9a36fd8f09b4c5ac141b2a3aa1
Aditya Patel <52347812+DeveloperAditya@users.noreply.github.com>
2021-04-25 17:17:09 +0530

    learned about columns in jetpack compose (#5)

--- app/src/main/java/com/adityatheoctocatdev/composeroadmap/ui/MainActivity.kt
+++ app/src/main/java/com/adityatheoctocatdev/composeroadmap/ui/MainActivity.kt
@@ -3,11 +3,21 @@ package com.adityatheoctocatdev.composeroadmap.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.adityatheoctocatdev.composeroadmap.R
 import com.adityatheoctocatdev.composeroadmap.ui.theme.ThemeAppearance.ComposeRoadmapTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +25,37 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeRoadmapTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                this.ColumnExample()
             }
         }
     }
 
-    @Composable
-    fun Greeting(name: String) {
-        Text(text = "Hello $name!")
-    }
-
     @Preview(showBackground = true)
     @Composable
-    fun DefaultPreview() {
-        ComposeRoadmapTheme {
-            Greeting("Android")
+    fun ColumnExample() {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color(0xFFF2F2F2))
+        ) {
+            items(count = 1) {
+                Image(
+                    painter = painterResource(id = R.drawable.happy_meal_small),
+                    contentDescription = null,
+                    modifier = Modifier.height(300.dp),
+                    contentScale = ContentScale.Crop
+                )
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(text = "Happy Meal", style = TextStyle(fontSize = 26.sp))
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(text = "800 Calories", style = TextStyle(fontSize = 17.sp))
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(
+                        text = "$5.99",
+                        style = TextStyle(fontSize = 17.sp, color = Color(0xFF85BB65))
+                    )
+                }
+            }
         }
     }
 }
