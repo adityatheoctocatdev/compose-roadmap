b115a0d502afeba430848f445ef8ce4bf93573b5
Aditya Patel <52347812+DeveloperAditya@users.noreply.github.com>
2021-05-02 17:15:11 +0530

    learned about rows in jetpack compose (#9)

--- app/src/main/java/com/adityatheoctocatdev/composeroadmap/ui/MainActivity.kt
+++ app/src/main/java/com/adityatheoctocatdev/composeroadmap/ui/MainActivity.kt
@@ -7,8 +7,10 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -25,7 +27,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeRoadmapTheme {
-                this.ColumnExample()
+                this.RowExample()
             }
         }
     }
@@ -54,6 +56,44 @@ class MainActivity : ComponentActivity() {
                         text = "$5.99",
                         style = TextStyle(fontSize = 17.sp, color = Color(0xFF85BB65))
                     )
+                }
+            }
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun RowExample() {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color(0xFFF2F2F2))
+        ) {
+            items(count = 1) {
+                Image(
+                    painter = painterResource(id = R.drawable.happy_meal_small),
+                    contentDescription = null,
+                    modifier = Modifier.height(300.dp),
+                    contentScale = ContentScale.Crop
+                )
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(text = "Happy Meal", style = TextStyle(fontSize = 26.sp))
+                        Text(
+                            text = "$5.99",
+                            style = TextStyle(fontSize = 17.sp, color = Color(0xFF85BB65)),
+                            modifier = Modifier.align(Alignment.CenterVertically)
+                        )
+                    }
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(text = "800 Calories", style = TextStyle(fontSize = 17.sp))
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Button(onClick = {}, modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                        Text(text = "Order Now")
+                    }
                 }
             }
         }
